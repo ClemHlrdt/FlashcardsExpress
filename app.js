8a675e6a5d8dbbace6ff3d89b9c0ab5c45b2b269
ClemHlrdt <clement.helardot@gmail.com>
2017-10-16 16:41:19 +0200

    Redirect and cookies

--- app.js
+++ app.js
@@ -1,35 +1,52 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 
-const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'purple'
-];
-
-const names = {
-        1: ["Daria"],
-        2: ["Clément"]
-}
-    
-
 app.set('view engine', 'pug');
-
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
 app.get("/", function (req, res) {
-    res.render('index');
-})
+    const name = req.cookies.username;
+    if(name){
+       res.render('index', {
+           name: req.cookies.username
+       });
+    } else {
+        res.redirect('hello');
+    }
+    
+});
+
+app.get("/hello", function (req, res) {
+    const name = req.cookies.username;
+    if(name){
+        console.log('cookie is here');
+        res.redirect('/');
+    } else {
+        res.render('hello');
+    }
+    
+});
+
+app.post('/goodbye', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('/hello');    
+});
+
+
+app.post("/hello", function (req, res) {
+    res.cookie('username', req.body.username);
+    res.redirect('/');
+});
 
 app.get("/cards", function (req, res) {
-    res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about who's tomb it is.", colors })
-})
+    res.render('/', { prompt: "Who is buried in Grant's tomb?", hint: "Think about who's tomb it is.", colors });
+});
 
 app.get("/sandbox", function (req, res) {
-    res.render('sandbox', { names })
-})
+    res.render('sandbox', { names });
+});
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!');
